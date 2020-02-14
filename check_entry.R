27688a1276d9880214674255e1cd9adc37e49680
parksw3 <parksw3@mcmaster.ca>
2020-02-14 17:05:18 -0500

    update check entry

--- check_entry.R
+++ check_entry.R
@@ -1,5 +1,8 @@
 library(readxl)
 
+covid1 <- read_xlsx("COVID19-Korea-2020-02-14.xlsx", sheet=1)
 covid2 <- read_xlsx("COVID19-Korea-2020-02-14.xlsx", sheet=2)
 
-which(covid2$positive + covid2$negative + covid2$unknown != covid2$`suspected cases`)
+all(covid2$positive + covid2$negative + covid2$unknown == covid2$`suspected cases`, na.rm=TRUE)
+
+all(2019 <= covid1$age + covid1$year_of_birth & covid1$age + covid1$year_of_birth <= 2020)
