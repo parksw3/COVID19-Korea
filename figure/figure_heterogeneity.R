6008e8c8ffce31d79383dc38819d600e3e6d1310
parksw3 <parksw3@mcmaster.ca>
2020-03-11 18:54:17 -0400

    update

---
+++ figure/figure_heterogeneity.R
@@ -0,0 +1,76 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2); theme_set(theme_bw())
+library(gridExtra)
+library(readxl)
+library(lubridate)
+source("../R/color_palette.R")
+source("../R/theme.R")
+
+covid2 <- read_xlsx("../COVID19-Korea-2020-03-11.xlsx", na="NA", sheet=2)
+covid8 <- read_xlsx("../COVID19-Korea-2020-03-11.xlsx", na="NA", sheet=8)
+
+covid2_diff <- covid2 %>%
+  mutate(
+    time_report=hour(time_report)
+  ) %>%
+  filter(is.na(time_report) | time_report != 16) %>%
+  mutate(sum=positive+negative,
+         sum=diff(c(0, sum)),
+         positive=diff(c(0, positive)),
+         negative=diff(c(0, negative)))
+
+
+covid8[is.na(covid8)] <- 0
+covid8$`Cheongdo Daenam Hospital`[6] <- NA
+
+covid8_diff <- covid8 %>%
+  mutate(
+    time_report=hour(time_report),
+    Shincheonji=diff(c(0, Shincheonji)),
+    `Cheongdo Daenam Hospital`=diff(c(0, `Cheongdo Daenam Hospital`)),
+    `Cheonan-si gym`=diff(c(0, `Cheonan-si gym`)),
+    `Guro call center`=diff(c(0, `Guro call center`))
+  )
+
+covid8_diff[is.na(covid8_diff)] <- 0
+
+mm <- merge(covid2_diff, covid8_diff) %>%
+  select(-time_report, -`suspected cases`, -negative, -discharged, -unknown, -death,
+         -`date_accessed (based on Korean time)`,
+         -`KCDC_no (https://www.cdc.go.kr/board/board.es?mid=a20501000000&bid=0015)`,
+         -sum,
+         -`Oncheon Church`) %>%
+  mutate(Other=positive-Shincheonji-`Cheongdo Daenam Hospital`-`Cheonan-si gym`-`Guro call center`) %>%
+  gather(key, value, -positive, -date_report) %>%
+  mutate(
+    value=ifelse(value < 0, 0, value),
+    key=factor(key, levels=c("Other", "Shincheonji", "Cheonan-si gym", "Cheongdo Daenam Hospital", "Guro call center"))
+  )
+
+g1 <- ggplot(mm) +
+  geom_bar(aes(date_report, value, fill=key), stat="identity") +
+  scale_x_datetime("Date reported") +
+  scale_y_continuous("Number of cases", expand=c(0, 0), limits=c(0, 1000)) +
+  scale_fill_manual(values=cpalette) +
+  btheme +
+  theme(
+    legend.title = element_blank(),
+    legend.position = c(0.2, 0.8)
+  )
+
+g2 <- ggplot(mm) +
+  geom_bar(aes(date_report, value, fill=key), stat="identity") +
+  scale_x_datetime("Date reported") +
+  scale_y_continuous("Number of cases", expand=c(0, 0), limits=c(0, NA)) +
+  scale_fill_manual(values=cpalette) +
+  btheme +
+  facet_wrap(~key, ncol=1, scale="free") +
+  theme(
+    legend.position = "none",
+    strip.background = element_blank()
+  )
+
+gtot <- arrangeGrob(g1, g2, nrow=1)
+
+ggsave("figure_heterogeneity.png", gtot, width=12, height=6)
--- figure/geographical_distribution.R
+++ figure/geographical_distribution.R
@@ -9,7 +9,7 @@ library(lubridate)
 source("../R/color_palette.R")
 source("../R/theme.R")
 
-covid3 <- read_xlsx("../COVID19-Korea-2020-03-01.xlsx", na="NA", sheet=3)
+covid3 <- read_xlsx("../COVID19-Korea-2020-03-11.xlsx", na="NA", sheet=3)
 
 geo_gather <- covid3 %>%
   gather(key, value, -date_report, -time_report, -total) %>%
@@ -34,18 +34,18 @@ geo_gather_sum <- geo_gather %>%
 
 geo_gather2 <- geo_gather %>%
   mutate(
-    key=factor(key, levels=geo_gather_sum$key)
+    key=factor(key, levels=geo_gather_sum$key),
+    value=ifelse(value < 0, 0, value)
   )
 
 g1 <- ggplot(geo_gather2) +
-  geom_line(aes(day, value), col="red") +
-  geom_point(aes(day, value), col="red") +
+  geom_bar(aes(date_report, value), fill=cpalette[6], stat="identity") +
   facet_wrap(~key, scale="free") +
-  scale_x_continuous("Days since Feb 18", breaks=0:6*2) +
-  scale_y_continuous("Daily number of reported cases") +
+  scale_x_datetime("Date reported", expand=c(0, 0)) +
+  scale_y_continuous("Number of cases", expand=c(0,0), limits=c(0, NA)) +
   btheme +
   theme(
     strip.background = element_blank()
   )
 
-ggsave("geographical_distribution.png", g1, width=9, height=9)
+ggsave("geographical_distribution.png", g1, width=12, height=12)
